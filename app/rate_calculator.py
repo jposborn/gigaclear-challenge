87dce756f298d497fdc969e50956c062e29ad759
John Osborn <jposborn@outlook.com>
2022-10-28 15:17:19 +0100

    Add Graphviz Diagram

--- app/rate_calculator.py
+++ app/rate_calculator.py
@@ -1,14 +1,15 @@
 import networkx as nx
 from networkx.algorithms.shortest_paths.generic import shortest_path
 from networkx.classes.function import path_weight
-
+import matplotlib.pyplot as plt
+from networkx.drawing.nx_pydot import graphviz_layout
 from collections import Counter
 
 
-def calc_distance_to_cabinet(source, target, G):
+def calc_distance_to_cabinet(source, target, g):
     """Calculate the distance between two points in network"""
-    path = shortest_path(G, source=source, target=target, weight="length")
-    path_length = path_weight(G, path, weight="length")
+    path = shortest_path(g, source=source, target=target, weight="length")
+    path_length = path_weight(g, path, weight="length")
 
     return path_length
 
@@ -26,13 +27,20 @@ def calc_network(rate_card, file):
     #  Use the Node data to count the number
     #  of items and identify the Pots and Cabinet
     items = []
+    node_labels = {}
     pots = []
+    node_colors = []
     for node in G.nodes(data=True):
         items.append(node[1]['type'])
         if node[1]['type'] == 'Pot':
             pots.append(node[0])
+            node_colors.append('Yellow')
         if node[1]['type'] == 'Cabinet':
             cabinet_id = node[0]
+            node_colors.append('Red')
+        if node[1]['type'] == 'Chamber':
+            node_colors.append('Blue')
+        node_labels[node[0]] = node[1]['type'][0:3].upper()
 
     item_count = Counter(items)
 
@@ -50,13 +58,46 @@ def calc_network(rate_card, file):
             dist_to_cab = calc_distance_to_cabinet(cabinet_id, pot, G)
             pot_cost = dist_to_cab * rate_card['Pot']
             pot_costs.append(pot_cost)
-            total_pot_cost = sum(pot_costs)
+        total_pot_cost = sum(pot_costs)
 
     #  Calculate total items costs and sum with trench costs to give total cost
     item_costs = [total_cabinet_cost, total_chamber_cost, total_pot_cost]
     total_cost = sum(item_costs) + sum(trench_costs)
 
+    draw_diagram(G, node_labels, node_colors)
+
     return total_cost
+
+
+def draw_diagram(g, node_labels, node_colors):
+    pos = graphviz_layout(g, prog="dot")
+    edge_label_length = nx.get_edge_attributes(g, 'length')
+    edge_label_material = nx.get_edge_attributes(g, 'material')
+
+    # # Build Edge Labels from material * length
+    # edge_labels = {edge: (edge_label_material[edge] + '\n(' + str(edge_label_length[edge])) + 'm)'
+    #                for edge in edge_label_material}
+
+    edge_labels = {}
+    edge_colors = []
+    for edge in edge_label_material:
+        label = edge_label_material[edge] + '\n(' + str(edge_label_length[edge]) + 'm)'
+        if edge_label_material[edge] == 'verge':
+            color = 'Green'
+        else:
+            color = "Black"
+        edge_labels.update({edge: label})
+        edge_colors.append(color)
+
+
+    # node_labels = nx.get_node_attributes(g, 'type')
+    #  nx.draw_networkx(g, pos, labels=node_labels, node_size=1000, node_color=node_colors)
+    nx.draw_networkx_nodes(g, pos, node_size=1000, node_color=node_colors, edgecolors='Black')
+    nx.draw_networkx_labels(g, pos, labels=node_labels)
+    nx.draw_networkx_edges(g, pos, edge_color=edge_colors, width=3)
+    nx.draw_networkx_edge_labels(g, pos, edge_labels=edge_labels)
+    plt.show()
+    return
 
 
 if __name__ == "__main__":
@@ -92,3 +133,4 @@ if __name__ == "__main__":
         'Total Network Cost (Rate Card B): £{:,.2f}'
         .format(total_network_cost_b)
         )
+
